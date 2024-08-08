925a737b58b45b92678713a3fd15ac988f25864c
Yohana <yohana.mfaria@gmail.com>
2024-08-08 01:41:31 -0300

    Deploy do projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,23 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "root",
-      password: "root",
-      database: "db_blog",
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
-      logging: true
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+        imports: [ConfigModule],
     }),
+    
     PostagemModule,
     TemaModule,
     AuthModule,
