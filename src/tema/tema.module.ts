0984ad08e530df45dd1d334930830afdffe3e2b7
yohanafaria <145880332+yohanafaria@users.noreply.github.com>
2024-08-08 02:00:40 -0300

    chore: tema.module

--- src/tema/tema.module.ts
+++ src/tema/tema.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Tema } from "./entities/tema.entity";
 import { TemaService } from "./services/tema.service";
-import { TemaController } from "./Controllers/tema.controller";
+import { TemaController } from "./controllers/tema.controller";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Tema])],
